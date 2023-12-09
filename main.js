99f255c8052d86989d6acad0ca63270f243bc2d5
Ho Nyeong <dev.honing@gmail.com>
2023-12-10 00:58:33 +0900

    keydown 이벤트로 테스트 콘솔 출력 완료

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+// DOM 제어를 위한 변수 선언
+var root = document.getElementById('root');
+var ball = document.querySelector('.ball');
+
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'AltLeft'){
+    jumpBall();
+  }
+})
+function jumpBall() {
+  console.log('테스트');
+}
